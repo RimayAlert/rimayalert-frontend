08b31c9c072654468edfb784a681388881ca4e09
FreddyAndres <fzambranoq2@unemi.edu.ec>
2025-10-24 01:03:41 -0500

    feat(splashscreen): :sparkles: implement splash screen with navigation and theme setup

--- app/src/main/java/com/fazq/rimayalert/MainActivity.kt
+++ app/src/main/java/com/fazq/rimayalert/MainActivity.kt
@@ -4,58 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.fazq.rimayalert.core.ui.navigation.NavGraph
 import com.fazq.rimayalert.core.ui.theme.RimayAlertTheme
-import com.fazq.rimayalert.features.auth.ui.LoginScreen
-import com.fazq.rimayalert.features.auth.views.ui.main.screen.RegisterScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RimayAlertTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var currentScreen by remember { mutableStateOf("login") }
-
-                    when (currentScreen) {
-                        "login" -> LoginScreen(
-                            onLoginClick = { email, password ->
-                                println("Login: $email")
-                            },
-                            onRegisterClick = {
-                                currentScreen = "register"
-                            },
-                            onForgotPasswordClick = {
-                                println("Recuperar contraseña")
-                            }
-                        )
-                        "register" -> RegisterScreen(
-                            onRegisterClick = { formData ->
-                                println("Registro: ${formData.username}, ${formData.email}")
-                            },
-                            onLoginClick = {
-                                currentScreen = "login"
-                            },
-                            onBackClick = {
-                                currentScreen = "login"
-                            },
-                            onTermsClick = {
-                                println("Abrir términos de uso")
-                            }
-                        )
-                    }
-                }
+                NavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/fazq/rimayalert/features/splash/ui/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.fazq.rimayalert.features.splash.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+    LaunchedEffect(Unit){
+        delay(2000)
+        navController.navigate("login") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Rimay Alert",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
